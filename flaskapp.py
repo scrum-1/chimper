6b77e3bbe6f67b7a768f7271474e4836cd1009cd
mdecourse <mdecourse@gmail.com>
2019-05-01 17:36:49 +0800

    add footer to colorlib

--- flaskapp.py
+++ flaskapp.py
@@ -897,6 +897,19 @@ def get_page2(heading, head, edit, get_page_content = None):
         return set_css2() + "<div class='container'><nav>"+ \
         directory + "<section><div id=\"tipue_search_content\">" + return_content + \
         '''</div></section></div>
+        
+    <!-- footer -->
+      <div class="container">
+        <div class="row">
+            <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+        </div>
+      </div>
+    <!-- for footer -->
+    
         </div> <!-- for site wrap -->
             <script src="../static/chimper/js/jquery-3.3.1.min.js"></script>
             <script src="../static/chimper/js/jquery-migrate-3.0.1.min.js"></script>
